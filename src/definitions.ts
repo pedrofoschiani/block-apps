b9d6a5f120f14308740914af7c303525c8c233d8
pedrofoschiani <pedro.foschiani@outlook.com>
2025-10-01 20:09:11 -0300

    iq

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,7 +1,8 @@
+import { registerPlugin } from '@capacitor/core';
+
 export interface BlockAppsPlugin {
-  /**
-   * Define a lista de pacotes de aplicativos que devem ser bloqueados.
-   * @param options - Um objeto contendo a lista de pacotes.
-   */
   setBlockedPackages(options: { packages: string[] }): Promise<void>;
 }
+
+const BlockApps = registerPlugin<BlockAppsPlugin>('BlockApps');
+export { BlockApps };
